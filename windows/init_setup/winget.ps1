c6c66fd694f1695f54244acfbccf8cb9722ae270
noice <43897965+infirms@users.noreply.github.com>
2026-01-30 17:51:53 +0300

    feat: remove pwsh from early installation to prevent Windows Terminal deadlock

--- windows/init_setup/winget.ps1
+++ windows/init_setup/winget.ps1
@@ -1,6 +1,5 @@
 winget update
 winget install --id=Microsoft.WindowsTerminal -e --accept-source-agreements
-winget install --id=Microsoft.PowerShell -e --accept-source-agreements
 winget install --id=VideoLAN.VLC -e --accept-source-agreements
 winget install --id=Giorgiotani.Peazip -e --accept-source-agreements
 winget install --id=DuongDieuPhap.ImageGlass -e --accept-source-agreements
