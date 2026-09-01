017eddd032f13ee3c97a1b11c10e7f1887cffe3e
inf <43897965+infirms@users.noreply.github.com>
2026-09-01 16:18:31 +0300

    feat(init_setup): updated scoop packages

--- windows/init_setup/scoop_admin.ps1
+++ windows/init_setup/scoop_admin.ps1
@@ -5,17 +5,23 @@ scoop bucket add versions
 scoop update
 
 scoop install main/aria2
+scoop install extras/vcredist-aio
 scoop install main/winget
 scoop install main/cmake
 scoop install main/ninja
 scoop install main/python
 scoop install main/clangd
 
-scoop install extras/vcredist-aio
 scoop install extras/bitwarden
 scoop install extras/vscode
+scoop install extras/zed
 scoop install extras/notesnook
-scoop install extras/teamspeak3
 scoop install extras/obs-studio
-scoop install extras/imhex
-scoop install extras/filezilla
+scoop install extras/winscp
+scoop install extras/winmerge
+scoop install main/llvm
+
+scoop install extras/telegram
+scoop install extras/teamspeak3
+
+scoop install main/oh-my-pi
